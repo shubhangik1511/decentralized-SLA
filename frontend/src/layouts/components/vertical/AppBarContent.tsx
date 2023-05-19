9a0507adbdcd013d658acd455a7d683e6c9581b7
Shubhangi Kumari <shubhangik1511@gmail.com>
2023-05-19 22:19:30 +0530

    removed unused imports.

--- frontend/src/layouts/components/vertical/AppBarContent.tsx
+++ frontend/src/layouts/components/vertical/AppBarContent.tsx
@@ -1,14 +1,11 @@
 // ** MUI Imports
 import Box from '@mui/material/Box'
 import { Theme } from '@mui/material/styles'
-import TextField from '@mui/material/TextField'
 import IconButton from '@mui/material/IconButton'
 import useMediaQuery from '@mui/material/useMediaQuery'
-import InputAdornment from '@mui/material/InputAdornment'
 
 // ** Icons Imports
 import Menu from 'mdi-material-ui/Menu'
-import Magnify from 'mdi-material-ui/Magnify'
 
 // ** Type Import
 import { Settings } from 'src/@core/context/settingsContext'
