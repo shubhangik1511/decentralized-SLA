7a6bbf1a6e2eed51b558b5656df6231374e64072
Shubhangi Kumari <shubhangik1511@gmail.com>
2023-05-20 19:18:01 +0530

    Added hooks for SLAs and Contracts.

--- frontend/src/views/view-sla/SLAsTable.tsx
+++ frontend/src/views/view-sla/SLAsTable.tsx
@@ -7,19 +7,11 @@ import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 
-const createData = (name: string, calories: number, fat: number, carbs: number, protein: number) => {
-  return { name, calories, fat, carbs, protein }
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9)
-]
+import useSLAs from 'src/@core/hooks/useSLAs'
 
 const SLAsTable = () => {
+  const { SLAs } = useSLAs()
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label='simple table'>
@@ -33,7 +25,7 @@ const SLAsTable = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map(row => (
+          {SLAs.map(row => (
             <TableRow
               key={row.name}
               sx={{
