af89752b7766b81b52b2093a325a4d5c6a2b1852
Shubhangi Kumari <shubhangik1511@gmail.com>
2023-05-20 16:35:57 +0530

    added create sla in navigation.

--- frontend/src/navigation/vertical/index.ts
+++ frontend/src/navigation/vertical/index.ts
@@ -1,14 +1,11 @@
 // ** Icon imports
-import Login from 'mdi-material-ui/Login'
 import Table from 'mdi-material-ui/Table'
 import CubeOutline from 'mdi-material-ui/CubeOutline'
 import HomeOutline from 'mdi-material-ui/HomeOutline'
 import FormatLetterCase from 'mdi-material-ui/FormatLetterCase'
-import AccountCogOutline from 'mdi-material-ui/AccountCogOutline'
 import CreditCardOutline from 'mdi-material-ui/CreditCardOutline'
-import AccountPlusOutline from 'mdi-material-ui/AccountPlusOutline'
-import AlertCircleOutline from 'mdi-material-ui/AlertCircleOutline'
 import GoogleCirclesExtended from 'mdi-material-ui/GoogleCirclesExtended'
+import PlusCircleOutline from 'mdi-material-ui/PlusCircleOutline'
 
 // ** Type import
 import { VerticalNavItemsType } from 'src/@core/layouts/types'
@@ -16,35 +13,14 @@ import { VerticalNavItemsType } from 'src/@core/layouts/types'
 const navigation = (): VerticalNavItemsType => {
   return [
     {
-      sectionTitle: 'Demo Pages'
-    },
-    {
       title: 'Dashboard',
       icon: HomeOutline,
       path: '/dashboard'
     },
     {
-      title: 'Account Settings',
-      icon: AccountCogOutline,
-      path: '/account-settings'
-    },
-    {
-      title: 'Login',
-      icon: Login,
-      path: '/login',
-      openInNewTab: true
-    },
-    {
-      title: 'Register',
-      icon: AccountPlusOutline,
-      path: '/register',
-      openInNewTab: true
-    },
-    {
-      title: 'Error',
-      icon: AlertCircleOutline,
-      path: '/error',
-      openInNewTab: true
+      title: 'Create SLA',
+      icon: PlusCircleOutline,
+      path: '/create-sla'
     },
     {
       sectionTitle: 'User Interface'
