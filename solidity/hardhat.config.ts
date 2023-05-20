0bb6b4353935a3d0bfe16f479b3251984f77b087
Shubhangi Kumari <shubhangik1511@gmail.com>
2023-05-20 22:53:30 +0530

    Some fixes to hardhat verify.

--- solidity/hardhat.config.ts
+++ solidity/hardhat.config.ts
@@ -2,19 +2,18 @@ import dotenv from "dotenv";
 dotenv.config();
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-verify";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
   networks: {
     hardhat: {},
-    polygon_mumbai: {
+    polygonMumbai: {
       url: "https://rpc-mumbai.maticvigil.com",
       accounts: [process.env.PRIVATE_KEY as string],
     },
   },
   etherscan: {
-    apiKey: { polygon_mumbai: process.env.POLYGONSCAN_API_KEY as string },
+    apiKey: { polygonMumbai: process.env.POLYGONSCAN_API_KEY as string },
   },
 };
 
