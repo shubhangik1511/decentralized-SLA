3e9190bbe40c47431b4a6b99766f68a79467cefc
Shubhangi Kumari <shubhangik1511@gmail.com>
2023-05-25 23:07:18 +0530

    Basic integration of view-sla and contracts done.

--- frontend/src/views/view-sla/SLAsTable.tsx
+++ frontend/src/views/view-sla/SLAsTable.tsx
@@ -7,50 +7,53 @@ import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 
-const createData = (name: string, calories: number, fat: number, carbs: number, protein: number) => {
-  return { name, calories, fat, carbs, protein }
+import { useContractRead, useAccount } from 'wagmi'
+import managerAbi from 'src/@core/abi/ManagerAbi.json'
+
+interface SLA {
+  name: string
+  slaAddress: string
+  createdAt: number
 }
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9)
-]
-
 const SLAsTable = () => {
+  const { address } = useAccount()
+  const { data, isError, isLoading } = useContractRead({
+    address: `0x${process.env.NEXT_PUBLIC_MANAGER_CONTRACT_ADDRESS}`,
+    abi: managerAbi,
+    functionName: 'getProviderSLAs',
+    args: [address]
+  })
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label='simple table'>
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align='right'>Calories</TableCell>
-            <TableCell align='right'>Fat (g)</TableCell>
-            <TableCell align='right'>Carbs (g)</TableCell>
-            <TableCell align='right'>Protein (g)</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell align='center'>Address</TableCell>
+            <TableCell align='right'>Created At</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map(row => (
-            <TableRow
-              key={row.name}
-              sx={{
-                '&:last-of-type td, &:last-of-type th': {
-                  border: 0
-                }
-              }}
-            >
-              <TableCell component='th' scope='row'>
-                {row.name}
-              </TableCell>
-              <TableCell align='right'>{row.calories}</TableCell>
-              <TableCell align='right'>{row.fat}</TableCell>
-              <TableCell align='right'>{row.carbs}</TableCell>
-              <TableCell align='right'>{row.protein}</TableCell>
-            </TableRow>
-          ))}
+          {!isError &&
+            !isLoading &&
+            (data as SLA[]).map(row => (
+              <TableRow
+                key={row.name}
+                sx={{
+                  '&:last-of-type td, &:last-of-type th': {
+                    border: 0
+                  }
+                }}
+              >
+                <TableCell component='th' scope='row'>
+                  {row.name}
+                </TableCell>
+                <TableCell align='center'>{row.slaAddress}</TableCell>
+                <TableCell align='right'>{row.createdAt}</TableCell>
+              </TableRow>
+            ))}
         </TableBody>
       </Table>
     </TableContainer>
