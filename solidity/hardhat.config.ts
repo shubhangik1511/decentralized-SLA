554d76d66507a9e4b4ba0458be7c00dfff05262f
Chaitanya Bankanhal <bchaitanya15@gmail.com>
2023-05-27 14:13:39 +0530

    Squashed commit of the following:
    commit 4097fb2c366d1880ee66df275ebea25b24366138
    Author: Chaitanya Bankanhal <bchaitanya15@gmail.com>
    Date:   Sat May 27 14:12:57 2023 +0530
    
        Add functions and other minor changes
    
    commit fb1bc9b7781854b946741e0fd7348f6b165b6c48
    Merge: 40705cd b512347
    Author: Chaitanya Bankanhal <bchaitanya15@gmail.com>
    Date:   Thu May 25 23:02:52 2023 +0530
    
        Merge remote-tracking branch 'origin/master' into add-chainlink-stuff
    
    commit 40705cdae1638a0ea2fc5634242d55b395719a7c
    Author: Chaitanya Bankanhal <bchaitanya15@gmail.com>
    Date:   Thu May 25 09:30:56 2023 +0530
    
        Add call, deploy consumer files
    
    commit 045176c9e270b5d2c1ea39b7d446a4ccf9cb4d2b
    Author: Chaitanya Bankanhal <bchaitanya15@gmail.com>
    Date:   Wed May 24 10:45:49 2023 +0530
    
        Add chainlink email validation stuff

--- solidity/hardhat.config.ts
+++ solidity/hardhat.config.ts
@@ -1,10 +1,36 @@
-import dotenv from "dotenv";
-dotenv.config();
+import { config as envEncConfig } from "@chainlink/env-enc";
+envEncConfig();
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const SOLC_SETTINGS = {
+  optimizer: {
+    enabled: true,
+    runs: 1_000,
+  },
+};
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.18",
+        settings: SOLC_SETTINGS,
+      },
+      {
+        version: "0.7.0",
+        settings: SOLC_SETTINGS,
+      },
+      {
+        version: "0.6.6",
+        settings: SOLC_SETTINGS,
+      },
+      {
+        version: "0.4.24",
+        settings: SOLC_SETTINGS,
+      },
+    ],
+  },
   networks: {
     hardhat: {},
     polygonMumbai: {
--- solidity/scripts/deploy.ts
+++ solidity/scripts/deploy.ts
@@ -1,13 +1,40 @@
-import { ethers } from "hardhat";
+import hre, { ethers } from "hardhat";
+import { networks } from "../networks";
+
+const networkName = hre.network.name as keyof typeof networks;
+const network = networks[networkName];
 
 async function main() {
   const ManagerContract = await ethers.getContractFactory("Manager");
   const manager = await ManagerContract.deploy(
-    "0x5f9c675c58432f40947AA66f43Bc639322F8f20d"
+    "0x5f9c675c58432f40947AA66f43Bc639322F8f20d",
+    "0xcC6D7f89f198DE64dbfA211C75De9bDcaBD67d22"
   );
   await manager.deployed();
-
   console.log(`Manager deployed to ${manager.address}`);
+
+  const slaContractTransaction = await (
+    await manager.createSLAContract("Test")
+  ).wait();
+  const slaContractAddress = slaContractTransaction.events?.find(
+    (event) => event.event === "SLAContractCreated"
+  )?.args?.[0];
+  const sla = await ethers.getContractAt("SLA", slaContractAddress);
+  console.log(`SLA deployed to ${sla.address}`);
+
+  const result = await (
+    await sla.inviteConsumer(
+      "hello",
+      ["bchaitanya15@gmail.com", "hello"],
+      1114,
+      300_000,
+      {
+        gasLimit: 1_500_000,
+        gasPrice: network.gasPrice,
+      }
+    )
+  ).wait(2);
+  console.log(result);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
